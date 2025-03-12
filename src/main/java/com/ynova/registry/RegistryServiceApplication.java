988e87fe0d338c8a5f1b78600a1d3dbb86feec82
Yeferson Nova <yeferson_nova@outlook.com>
2025-03-11 23:22:20 -0500

    Update project dependencies and configurations
    - Update pom.xml dependencies
    - Update application.yml configuration

--- src/main/java/com/ynova/registry/RegistryServiceApplication.java
+++ src/main/java/com/ynova/registry/RegistryServiceApplication.java
@@ -2,9 +2,10 @@ package com.ynova.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-
+@EnableEurekaServer
 public class RegistryServiceApplication {
 
 	public static void main(String[] args) {
